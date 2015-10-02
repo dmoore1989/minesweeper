78091b1939dc6d26a23e34ab62e3532d84b19e51
Douglas Moore <douglas.moore89@gmail.com>
2015-10-02 17:24:37 -0400

    Add save & load functionality

--- Minesweeper.rb
+++ Minesweeper.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require_relative 'Board'
 require 'byebug'
 
@@ -19,7 +20,12 @@ class Game
         input = get_move
       end
 
-      if input.include?("f")
+
+      if input == "save"
+        save_f
+      elsif input == "load"
+        load_f
+      elsif input.include?("f")
         @board[input.last].toggle_flag
       else
         take_move(input.last)
@@ -55,17 +61,33 @@ class Game
 
   def get_move
     puts "Please enter your move. Type f before the position to flag or unflag"
+    puts "Type save to save your game, load to load your game"
     move = gets.chomp.downcase
+    return move if move == "save" || move == "load"
     move.scan("f") << move.scan(/\d/)[0, 2].map(&:to_i)
   end
 
   def valid_move?(input)
     # byebug
+    return true if input == "save" || input == "load"
     input.uniq!
+
     return false if input.last.length != 2
     return false if input.first != "f" && @board[input.last].flagged
 
     input.last.length == 2 && @board[input.last].state == :hidden
 
+  end
+
+  def save_f
+    save_file = self.to_yaml
+    File.open("minesweeper.txt", "w") do |f|
+      f.puts save_file
+    end
+  end
+
+  def load_f
+    load_file = File.read("minesweeper.txt")
+    YAML.load(load_file).play
   end
 end
