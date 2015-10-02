a213e75741f557b76d396d447ee49028e36b22a4
Douglas Moore <douglas.moore89@gmail.com>
2015-10-02 11:56:31 -0400

    Add check_valid_neighbors method

--- Board.rb
+++ Board.rb
@@ -1,15 +1,16 @@
 require_relative 'Tile'
 
 class Board
-  BOARD_SIZE = 8
+  BOARD_SIZE = 9
   MINES = 10
+  MOVES =[[1,0], [1,1], [0,1], [-1,0], [-1,-1], [0,-1], [1,-1], [-1,1]]
 
   def initialize
     @grid = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) {Tile.new}}
-    place_bombs
+    place_mines
   end
 
-  def place_bombs
+  def place_mines
     mine_count = 0
     until mine_count == MINES
       mine_tile = @grid.sample.sample
@@ -18,6 +19,13 @@ class Board
         mine_count += 1
       end
     end
+  end
+
+  def check_valid_neighbors(position)
+    moves = MOVES.map do |move|
+      [move.first + position.first, move.last + position.last]
+    end
+    moves.select{ |move| move.all? { |pos| pos.between?(0, BOARD_SIZE - 1) }}
   end
 
 end
